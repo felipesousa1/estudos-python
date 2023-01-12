90ad32924a6a72f33e4feeaaf5d8bf0bcff20c48
Felipe Sousa <felipesousa1@github.com>
2023-01-12 09:23:23 -0300

    melhorando logica do fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,10 @@
 n = int(input("Digite o n valor de fibonacci: "))
 
 def Fibonacci (n):
-    if n == 0:
-        return 1
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
     
     return Fibonacci(n-2) + Fibonacci (n-1)
 
-print(Fibonacci(n))
+for i in range (1,n+1):
+    print(Fibonacci(i))
